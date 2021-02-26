94e171adae89cf2a08bb3f76419a339c46fa7bf8
Yago <yal.yep@gmail.com>
2021-02-26 15:05:32 -0300

    kimmy

---
+++ src/pages/components/Profile.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://pbs.twimg.com/profile_images/846118214115479552/vaU-yKqv_400x400.jpg" alt="Yago"/>
+        
+        <div>
+            <strong>Yago Almeida</strong>
+            <p>
+                <img src="icons/level.svg" alt="Level"/>    
+                Level {level}
+            </p>
+        </div>
+        </div>
+    
+    );
+}
